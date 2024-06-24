b3a1d5e0843e6b1a46c567a0d2ecddc4751f202f
EngFaizan <eng.faizan.yousaf@gmail.com>
2024-06-24 18:51:34 +0500

    Added Abstract Class ThreeDimensionalShape.java

---
+++ src/ThreeDimensionalShape.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author lenovo
+ */
+public abstract class ThreeDimensionalShape extends Shape{
+    public int getNumberofDimensions(){
+        return 3;
+    }
+    
+    public abstract double getvolume();
+}
